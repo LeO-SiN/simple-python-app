e58bae375c23d3cbee9e52739612c61a9f4db4bf
Pratyush Mishra <96896564+LeO-SiN@users.noreply.github.com>
2024-08-15 12:19:30 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull pratyushmishra82160/simple-python-flask-app
 
 # Run the Docker image as a containeecho
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 pratyushmishra82160/simple-python-flask-app
